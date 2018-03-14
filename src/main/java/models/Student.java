1a0394eb9b1adfa51948b3b3c696fe7ea027287d
Christiebelle <cslester@outlook.com>
2018-03-14 13:24:49 +0000

    Student class set up

--- src/main/java/models/Student.java
+++ src/main/java/models/Student.java
@@ -1,4 +1,71 @@
 package models;
 
+import java.util.Set;
+
 public class Student {
+
+    private int id;
+    private String name;
+    private int age;
+    private int studentno;
+    private Course course;
+    private Set<Lesson> lessons;
+
+    public Student() {
+    }
+
+    public Student(String name, int age, int studentno, Course course) {
+        this.name = name;
+        this.age = age;
+        this.studentno = studentno;
+        this.course = course;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getStudentno() {
+        return studentno;
+    }
+
+    public void setStudentno(int studentno) {
+        this.studentno = studentno;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Set<Lesson> getLessons() {
+        return lessons;
+    }
+
+    public void setLessons(Set<Lesson> lessons) {
+        this.lessons = lessons;
+    }
 }
