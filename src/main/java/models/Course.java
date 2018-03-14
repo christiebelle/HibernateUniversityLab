22a8c598e61012ccf72fdbc85f1107877907c3bc
Christiebelle <cslester@outlook.com>
2018-03-14 13:21:04 +0000

    Course class set up

--- src/main/java/models/Course.java
+++ src/main/java/models/Course.java
@@ -1,4 +1,51 @@
 package models;
 
+import java.util.Set;
+
 public class Course {
+
+    private int id;
+    private String title;
+    private String level;
+    private Set<Student> students;
+
+    public Course() {
+    }
+
+    public Course(String title, String level) {
+        this.title = title;
+        this.level = level;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
+    }
 }
