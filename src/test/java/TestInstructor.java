010bfc593282be63a94943c9e0918c04f012129c
Christiebelle <cslester@outlook.com>
2018-03-15 09:57:11 +0000

    Amendments made to the Instructor/Lessons relationship to get this working

--- src/test/java/TestInstructor.java
+++ src/test/java/TestInstructor.java
@@ -1,2 +1,33 @@
+import db.DBHelper;
+import models.Course;
+import models.Instructor;
+import models.Lesson;
+import models.Student;
+import org.junit.After;
+import org.junit.Before;
+
 public class TestInstructor {
+
+    Instructor instructor;
+    Course course;
+    Student student;
+    Lesson lesson;
+
+    @After
+    public void tearDown() throws Exception {
+        DBHelper.delete(instructor);
+        DBHelper.delete(student);
+        DBHelper.delete(lesson);
+        DBHelper.delete(course);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        instructor = new Instructor("Queen Elizabeth");
+        DBHelper.save(instructor);
+        course = new Course("");
+        DBHelper.save(course);
+        student = new Student("Portia", 19, 1605, course);
+        DBHelper.save(instructor);
+    }
 }
